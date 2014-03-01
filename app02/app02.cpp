b2163b0f09d3000bb700279c9a88de48568becb4
andyferraro <andrea.ferraro@cnaf.infn.it>
2014-03-01 22:06:05 +0100

    test

---
+++ app02/app02.cpp
@@ -0,0 +1,14 @@
+// app02.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "app02";
+	return 0;
+}
+
