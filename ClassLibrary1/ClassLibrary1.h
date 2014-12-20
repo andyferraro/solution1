40bdb3945879ba8b538ca615f7f719954eeab1e7
Andrea Ferraro <andrea.ferraro@cnaf.infn.it>
2014-12-20 11:12:16 +0100

    new2

---
+++ ClassLibrary1/ClassLibrary1.h
@@ -0,0 +1,13 @@
+// ClassLibrary1.h
+
+#pragma once
+
+using namespace System;
+
+namespace ClassLibrary1 {
+
+	public ref class Class1
+	{
+		// TODO: aggiungere qui i metodi per la classe.
+	};
+}
